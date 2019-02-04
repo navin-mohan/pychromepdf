af1e912fc9bef7fc85597ee12aa27e6d19867cfa
Navin Mohan <navinmohan81@gmail.com>
2019-02-04 22:33:06 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md','r') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='pychromepdf',
+    version='0.0.1',
+    author='Navin Mohan',
+    author_email='navinmohan81@gmail.com',
+    description='Creates PDFs from HTML rendered using chrome or chromium',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nvnmo/pychromepdf',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+)
